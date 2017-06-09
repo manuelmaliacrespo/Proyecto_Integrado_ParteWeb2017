6658a8053ddca22afe7ea6671ae837d186494ffe
mmalia <mike_spriggan@hotmail.com>
2017-06-09 09:39:32 +0200

    primeros pasos

--- instalacion.php
+++ instalacion.php
@@ -7,30 +7,70 @@
 </head>
 <body>
 
+
+
+<?php 
+//PASO 1 
+$_GET["paso"] = 1;
+?>
+
+
 <div class="container">
 
 	<img src="images/logo.png" style="width: 400px"></img>
-	<form>
+
+
+
+
+	<?php 
+
+		//PRIMERA PARTE DE LA INSTALACIÓN
+		if (isset($_POST['comprobar'])) {
+			
+			$connection = new mysqli($_POST["host"], $_POST["usuario"], $_POST["clave"], $_POST["nombre"]);
+
+
+			if ($connection->connect_errno) {
+	              echo "No se ha podido conectar a la base de datos";
+	              echo "<br><br>";
+
+	        } else {
+
+	        	$_GET["paso"]=2;
+
+	        }
+
+		
+		}
+
+	?>
+
+
+
+	<?php if($_GET["paso"]==1): ?>
+		
+	<form method="post">
 
 	Nombre de la base de datos:<br>
-	<input name="nombre"></input>
+	<input name="nombre" required></input>
 	<br><br>
 
 	Host donde se encuentra:<br>
-	<input name="host"></input>
+	<input name="host" required></input>
 	<br><br>
 
 	Usuario:<br>
-	<input name="usuario"></input>
+	<input name="usuario" required></input>
 	<br><br>
 
 	Contraseña:<br>
-	<input name="clave"></input>
+	<input name="clave" required></input>
 
 	<br><br>
-	<input type="submit" value="Editar" class="btn btn-primary" name="comprobar">
+	<input type="submit" value="Comprobar Datos" class="btn btn-primary" name="comprobar">
 
 	</form>
+	<?php endif; ?>
 
 </div>
 
