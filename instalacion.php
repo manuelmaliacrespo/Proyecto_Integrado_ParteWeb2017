169463bb8b59b6e9f3931ade32b67a11df00fb6c
mmalia <mike_spriggan@hotmail.com>
2017-06-09 08:31:24 +0200

    creando fichero de instalación

---
+++ instalacion.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+ <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <link href="css/navbar.css" rel="stylesheet">
+    <script src="js/ie-emulation-modes-warning.js"></script>
+</head>
+<body>
+
+<div class="container">
+
+	<img src="images/logo.png" style="width: 400px"></img>
+	<form>
+
+	Nombre de la base de datos:<br>
+	<input name="nombre"></input>
+	<br><br>
+
+	Host donde se encuentra:<br>
+	<input name="host"></input>
+	<br><br>
+
+	Usuario:<br>
+	<input name="usuario"></input>
+	<br><br>
+
+	Contraseña:<br>
+	<input name="clave"></input>
+
+	<br><br>
+	<input type="submit" value="Editar" class="btn btn-primary" name="comprobar">
+
+	</form>
+
+</div>
+
+
+
+</body>
+</html>
